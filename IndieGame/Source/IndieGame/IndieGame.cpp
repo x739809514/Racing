acc2c2648d20b6eea6508af9617d3d61495e1563
Inno <1612650023@qq.com>
2024-07-05 12:05:22 +0100

    first

---
+++ IndieGame/Source/IndieGame/IndieGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "IndieGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, IndieGame, "IndieGame" );
